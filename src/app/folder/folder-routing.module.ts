45d5aae5f640d5bc2b79f2683e01cf3cd50b5fc1
SouAle <smc_gaytan@hotmail.com>
2021-03-30 00:53:57 -0600

    Ajustes del Menu: Routing

--- src/app/folder/folder-routing.module.ts
+++ src/app/folder/folder-routing.module.ts
@@ -7,7 +7,31 @@ const routes: Routes = [
   {
     path: '',
     component: FolderPage
+  },
+  {
+    path: 'Inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'Configuracion',
+    loadChildren: () => import('./configuracion/configuracion.module').then( m => m.ConfiguracionPageModule)
+  },
+  {
+    path: 'Analisis',
+    loadChildren: () => import('./datos/datos.module').then( m => m.DatosPageModule)
+  },
+  {
+    path: 'Proyecto',
+    loadChildren: () => import('./informacion/informacion.module').then( m => m.InformacionPageModule)
+  },
+  {
+    path: 'Registros',
+    loadChildren: () => import('./registros/registros.module').then( m => m.RegistrosPageModule)
   }
+
+
+
+
 ];
 
 @NgModule({
