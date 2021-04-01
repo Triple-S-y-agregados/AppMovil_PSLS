573d4bc4f6e4f9515dcaaa6025e5be3034412e85
Vicente Garza Reyna <vicente.garzara@uanl.edu.mx>
2021-04-01 00:00:27 -0600

    Moved graph from 'datos' to 'registros'

--- src/app/folder/datos/datos.module.ts
+++ src/app/folder/datos/datos.module.ts
@@ -7,17 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { DatosPageRoutingModule } from './datos-routing.module';
 
 import { DatosPage } from './datos.page';
-import { GraphingComponent } from 'src/app/components/graphing/graphing.component'
-import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    DatosPageRoutingModule,
-    ChartsModule
+    DatosPageRoutingModule
   ],
-  declarations: [DatosPage, GraphingComponent]
+  declarations: [DatosPage]
 })
 export class DatosPageModule {}
--- src/app/folder/registros/registros.module.ts
+++ src/app/folder/registros/registros.module.ts
@@ -7,14 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { RegistrosPageRoutingModule } from './registros-routing.module';
 
 import { RegistrosPage } from './registros.page';
+import { GraphingComponent } from 'src/app/components/graphing/graphing.component'
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RegistrosPageRoutingModule
+    RegistrosPageRoutingModule,
+    ChartsModule
   ],
-  declarations: [RegistrosPage]
+  declarations: [RegistrosPage, GraphingComponent]
 })
 export class RegistrosPageModule {}
