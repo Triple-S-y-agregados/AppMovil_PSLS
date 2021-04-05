5c794ef4a489045b47cceeb267d111f3d91b3314
SouAle <smc_gaytan@hotmail.com>
2021-04-05 16:44:50 -0500

    Cookie config

--- src/app/folder/configuracion/configuracion.page.ts
+++ src/app/folder/configuracion/configuracion.page.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DataTransferService } from 'src/app/services/data-transfer.service'
 import { AlertController } from '@ionic/angular'
+import { CookiesService } from 'src/app/services/cookie-service.service';
 
 @Component({
   selector: 'app-configuracion',
@@ -10,15 +11,35 @@ import { AlertController } from '@ionic/angular'
 export class ConfiguracionPage implements OnInit {
   @Input()
   public ip: string;
+  public manualMode: boolean = false;
 
   constructor( 
     private dtService: DataTransferService,
-    private alertCtrl: AlertController ) { }
+    private alertCtrl: AlertController, private cookies: CookiesService ) { }
 
   // Cookie format
   // config = "{ \"manualMode\":false, \"ipAddress\":\"0.0.0.0\" }";
   ngOnInit() { 
     this.ip = this.dtService.getIP();
+    this.modificarCookie();
+    let allCookies = document.cookie;
+    alert(allCookies);
+    this.leerCookie();
+  }
+
+  modificarCookie(){
+    let config: string = 
+      //"manualMode:"+
+      this.manualMode+","+
+      //"ipAddress:"+
+      this.ip;
+    this.cookies.setCookie("config", config, 1);
+  }
+
+  leerCookie(){
+    let config: string = this.cookies.getCookie("config");
+    let data: string[] = config.split(",");
+    alert("Modo Manual: " + data[0] + "\nIP Address: " + data[1]);
   }
 
   saveIP(){
---
+++ src/app/services/cookie-service.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CookiesService {
+
+  constructor() { }
+
+  setCookie(cookieName: string, cookieValue: string, daysToExpire: number) : void {
+    //Agrega una cookie con el nombre y el valor especificado.
+    let date: Date = new Date();
+    let expires: string = "expires=";
+    date.setTime(date.getTime() + daysToExpire * 24 * 60 * 60 * 1000);
+    expires += date.toISOString();
+
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+  }
+
+  getCookie(cookieName : string) : string {
+    //Regresa el valor de la cookie indicada.
+    let allCookies : string[] = decodeURIComponent(document.cookie).split(";");
+
+    for(let cookie of allCookies) {
+      let temp : string[] = cookie.split("=");
+      
+      if(temp[0].charAt(0) == ' ')
+        temp[0] = temp[0].substr(1);
+
+      if(temp[0] == cookieName) {
+        return temp[1];
+      }
+    }
+
+    return "";
+  }
+}
